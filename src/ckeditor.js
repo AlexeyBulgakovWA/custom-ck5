a9c7df7be52cff14741b61a4e32c1fc6bd6f0341
AlexeyBulgakovWA <70520841+AlexeyBulgakovWA@users.noreply.github.com>
2022-06-03 17:30:45 +0300

    Updated

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -5,8 +5,10 @@
 import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor.js';
 import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment.js';
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat.js';
+import Base64UploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/base64uploadadapter.js';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote.js';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
+import CloudServices from '@ckeditor/ckeditor5-cloud-services/src/cloudservices.js';
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
 import FontBackgroundColor from '@ckeditor/ckeditor5-font/src/fontbackgroundcolor.js';
 import FontColor from '@ckeditor/ckeditor5-font/src/fontcolor.js';
@@ -15,17 +17,21 @@ import FontSize from '@ckeditor/ckeditor5-font/src/fontsize.js';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading.js';
 import Image from '@ckeditor/ckeditor5-image/src/image.js';
 import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption.js';
+import ImageInsert from '@ckeditor/ckeditor5-image/src/imageinsert.js';
 import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle.js';
 import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar.js';
 import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload.js';
 import Indent from '@ckeditor/ckeditor5-indent/src/indent.js';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
 import Link from '@ckeditor/ckeditor5-link/src/link.js';
+import LinkImage from '@ckeditor/ckeditor5-link/src/linkimage.js';
 import List from '@ckeditor/ckeditor5-list/src/list.js';
+import ListProperties from '@ckeditor/ckeditor5-list/src/listproperties.js';
 import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed.js';
+import MediaEmbedToolbar from '@ckeditor/ckeditor5-media-embed/src/mediaembedtoolbar.js';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice.js';
-import SourceEditing from '@ckeditor/ckeditor5-source-editing/src/sourceediting.js';
+import RemoveFormat from '@ckeditor/ckeditor5-remove-format/src/removeformat.js';
 import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough.js';
 import Subscript from '@ckeditor/ckeditor5-basic-styles/src/subscript.js';
 import Superscript from '@ckeditor/ckeditor5-basic-styles/src/superscript.js';
@@ -40,8 +46,10 @@ class Editor extends ClassicEditor {}
 Editor.builtinPlugins = [
 	Alignment,
 	Autoformat,
+	Base64UploadAdapter,
 	BlockQuote,
 	Bold,
+	CloudServices,
 	Essentials,
 	FontBackgroundColor,
 	FontColor,
@@ -50,17 +58,21 @@ Editor.builtinPlugins = [
 	Heading,
 	Image,
 	ImageCaption,
+	ImageInsert,
 	ImageStyle,
 	ImageToolbar,
 	ImageUpload,
 	Indent,
 	Italic,
 	Link,
+	LinkImage,
 	List,
+	ListProperties,
 	MediaEmbed,
+	MediaEmbedToolbar,
 	Paragraph,
 	PasteFromOffice,
-	SourceEditing,
+	RemoveFormat,
 	Strikethrough,
 	Subscript,
 	Superscript,
@@ -74,29 +86,23 @@ Editor.builtinPlugins = [
 Editor.defaultConfig = {
 	toolbar: {
 		items: [
-			'fontFamily',
-			'fontSize',
 			'heading',
+			'|',
 			'bold',
 			'italic',
-			'underline',
-			'strikethrough',
-			'fontColor',
-			'fontBackgroundColor',
-			'alignment',
-			'numberedList',
+			'link',
 			'bulletedList',
+			'numberedList',
+			'|',
 			'outdent',
 			'indent',
-			'subscript',
-			'superscript',
-			'link',
+			'|',
 			'imageUpload',
 			'blockQuote',
 			'insertTable',
+			'mediaEmbed',
 			'undo',
-			'redo',
-			'sourceEditing'
+			'redo'
 		]
 	},
 	language: 'en',
@@ -105,7 +111,8 @@ Editor.defaultConfig = {
 			'imageTextAlternative',
 			'imageStyle:inline',
 			'imageStyle:block',
-			'imageStyle:side'
+			'imageStyle:side',
+			'linkImage'
 		]
 	},
 	table: {
